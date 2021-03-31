08f718ab2133b9b67712039125c7a7b9788a93fb
Sekhar Babu Patnam <babuthetrainer@gmail.com>
2021-03-31 08:00:38 +0530

    Spring boot JPA with Mysql/MSSql Server Demo

---
+++ ts-sb-jpa-mysql-demo/src/main/java/com/ts/sb/jpa/mysql/demo/controllers/UserManagementController.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package com.ts.sb.jpa.mysql.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ts.sb.jpa.mysql.demo.model.User;
+import com.ts.sb.jpa.mysql.demo.service.UserManagementService;
+
+/**
+ * @author dorak
+ *
+ */
+@RestController
+@RequestMapping("users")
+public class UserManagementController {
+	
+	@Autowired
+	private UserManagementService userManagementService;
+
+	// http://localhost:8090/users/all - GET
+	//@RequestMapping(value = "/all", method = RequestMethod.GET)
+	@GetMapping
+	public List<User> findAllUsers() {
+		// return usersList;
+		return userManagementService.findAllUsers();
+
+	}
+	
+	@GetMapping("/{id}")
+	public User findUserById(@PathVariable(name = "id") Long userId) throws Exception {
+		return userManagementService.findUserById(userId);
+	}
+	
+	@PostMapping
+	public User addNewUser(@RequestBody User user) {
+		return userManagementService.addNeUser(user);
+	}
+	
+	@PutMapping
+	public User updateUser(@RequestBody User user) throws Exception {
+		return userManagementService.updateUser(user);
+	}
+	
+	@DeleteMapping("/{id}")
+	public String deleteUser(@PathVariable Long id) {
+		return userManagementService.deleteUser(id);
+	}
+}
---
+++ ts-sb-jpa-mysql-demo/src/main/java/com/ts/sb/jpa/mysql/demo/model/User.java
@@ -0,0 +1,87 @@
+/**
+ * 
+ */
+package com.ts.sb.jpa.mysql.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author dorak
+ *
+ */
+@Entity
+@Table(name = "ts_users")
+public class User {
+
+	@Id
+	private Long userId;
+	private String userName;
+	private String firstName;
+	private String lastName;
+	private String password;
+	
+	/**
+	 * @return the userId
+	 */
+	public Long getUserId() {
+		return userId;
+	}
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+		
+}
